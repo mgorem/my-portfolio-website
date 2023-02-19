b7df18fad335ab698c8deb9331d4af50a5df7331
omg <oremgitonga22@gmail.com>
2023-02-19 19:34:13 +0300

    navbar begin

---
+++ components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Image from "next/image";
+
+const Navbar = () => {
+  return (
+    <div className="fixed w-full h-20 shadow-xl z-[100]">
+      <div
+        className="flex justify-between items-center 
+                    w-full h-full px-2"
+      >
+        <Image
+          src="/../public/assets/orem-gitonga.png"
+          alt=""
+          width="150"
+          height="50"
+          style={{ objectFit: "cover" }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
