d8e2d6f70c04e8742c5e0ef8291a560b2493cc3f
omg <oremgitonga22@gmail.com>
2023-02-19 19:52:14 +0300

    change image from png to h2

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -8,13 +8,16 @@ const Navbar = () => {
         className="flex justify-between items-center 
                     w-full h-full px-2"
       >
-        <Image
+        <h2>
+          <span className="text-orange-500">OREM</span> GITONGA
+        </h2>
+        {/* <Image
           src="/../public/assets/orem-gitonga.png"
           alt=""
           width="150"
           height="50"
           style={{ objectFit: "cover" }}
-        />
+        /> */}
       </div>
     </div>
   );
